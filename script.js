c3a39a40388898c7c850e008f006a5c8b019861b
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-02-18 13:37:54 -0400

    Finish all Carousel section

---
+++ script.js
@@ -0,0 +1,60 @@
+particlesJS("particles-js", {
+  "particles": {
+      "number": {
+          "value": 120
+      },
+      "color": {
+          "value": "#ffffff"
+      },
+      "shape": {
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#f2f3f6"
+          }
+      },
+      "polygon": {
+          "nb_sides": 3
+      },
+      "opacity": {
+          "value": 0.8,
+          "random": false,
+          "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+          }
+      }
+  }
+});
